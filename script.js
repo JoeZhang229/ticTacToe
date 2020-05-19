8922101d9a282ca5271f70bdee11d2e4600dd275
Joe Zhang <jozhang123@gmail.com>
2020-05-18 21:03:29 -0700

    simplification and tidied up for readability

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Player = (name, mark) => {
     let moves = [];
+    let turn = "";
     return { name, mark, moves };
 };
 
@@ -8,55 +9,68 @@ let player2 = Player("player2", "O");
 
 const gameBoard = (() => {
     let turn = 0;
+    const maxTurns = 9;
     const board = [ "", "", "", "", "", "", "", "", "" ];
     const callBoard = () => board;
     const htmlBoard = document.querySelector(".gameBoard");
-    let players = [];
     const result = document.querySelector(".result");
 
     function randStart() {
         // randomly changes player array to sync up turn & player marks
         if (Math.random() > 0.5) {
-            players.push(player1)
-            players.push(player2)
-            return player1
+            return player1;
         } else {
-            players.push(player2)
-            players.push(player1)
-            return player2
+            return player2;
         }
     }
 
-    function playerActions(player, e) {
+    function playerMarking(player, e) {
         e.target.textContent = player.mark;
         player.moves.push(Number(e.target.id));
     }
-    
-    const gameStart = () => {
-        randStart();
-        result.textContent = `${players[0].name}'s turn`;
+
+    function stateTurn(player) {
+        player.turn = true;
+        result.textContent = `${player.name}'s turn`;
+    }
+
+    const startGame = () => {
+        stateTurn(randStart());
+
         board.forEach((cell, index) => {
-            let cells = document.createElement("div")
-            cells.className = "gameBoardCell"
-            cells.id = `${index}`
-            cells.textContent = board[index]
+            let cells = document.createElement("div");
+
+            cells.className = "gameBoardCell";
+            cells.id = `${index}`;
+            cells.textContent = board[index];
+
             cells.addEventListener("click", (e) => {
                 // player's turn and marks are alternating with every turn
+                
                 if (e.target.textContent === "") {
-                    let nextPlayer = players[turn % 2]
-                    if (turn === 0) {
-                        playerActions(players[turn % 2], e);
-                    } else {
-                        playerActions(players[turn % 2], e);
+
+                    if (player1.turn === true) {
+                        playerMarking(player1, e);
+                        stateTurn(player2);
+                        console.log("player 1", player1.turn)
+                        player1.turn = false;
+
+                    } else if (player2.turn === true) {
+                        playerMarking(player2, e);
+                        stateTurn(player1);
+                        console.log("player 2", player2.turn)
+                        player2.turn = false;
                     }
+
                     turn++;
-                    result.textContent = `${players[turn % 2].name}'s turn`;
                 }
+
                 if (gameStatus(player1.moves, player2.moves)) {
                     result.textContent = gameStatus(player1.moves, player2.moves)
                     htmlBoard.style.display = "none"
                 }
             });
+
             htmlBoard.appendChild(cells)
         })
     }
@@ -71,30 +85,36 @@ const gameBoard = (() => {
         [0, 4, 8],
         [6, 4, 2]
     ];
+
+    const checkIfWinner = (playerMoves) => 
+        winCombos.some((subArrays) =>
+            subArrays.every((index) => playerMoves.includes(index))
+        );
     
     const gameStatus = (player1Moves, player2Moves) => {
-        if (winner(player1Moves)) {
-            return `${player1.name} Wins!`
-        } else if (winner(player2Moves)) {
-            return `${player2.name} Wins!`
-        } else if (turn >= 9){
-            return "It's a Tie!"
+        if (checkIfWinner(player1Moves)) {
+            return `${player1.name} Wins!`;
+
+        } else if (checkIfWinner(player2Moves)) {
+            return `${player2.name} Wins!`;
+
+        } else if (turn == maxTurns) {
+            return "It's a Tie!";
         }
     }
 
-    const winner = (playerMoves) =>
-        winCombos.some((combos) =>
-        combos.every((allMoves) => playerMoves.includes(allMoves))
-        );
     let gameStarted = false;
+    
     const startGameButton = () => {
         let playerForm = document.querySelector("form");
         let startButton = document.querySelector(".start");
+
         startButton.addEventListener("click", (e) => {
             if ((playerForm[0].value && playerForm[1].value) && !gameStarted) {
+
                 player1.name = playerForm[0].value
                 player2.name = playerForm[1].value
-                gameStart();
+                startGame();
                 gameStarted = true;
             }
         });
@@ -103,24 +123,27 @@ const gameBoard = (() => {
 
     const resetGame = () => {
         const allCells = document.querySelectorAll(".gameBoardCell");
+
         allCells.forEach(cell => cell.textContent = "")
+
         player1.moves = [];
         player2.moves = [];
-        players = [];
         turn = 0;
-        randStart();
-        result.textContent = `${players[0].name}'s turn`;
+        stateTurn(randStart());
     }
 
     const restartButton = () => {
         let restartButton = document.querySelector(".restart");
+
         restartButton.addEventListener("click", function() {
+            
             result.textContent = "";
             htmlBoard.style.display = "grid";
             resetGame();
         });
     }
+
     restartButton();
 
-    return { board, callBoard, gameStatus, gameStart, startGameButton, resetGame, restartButton }
+    return { board, callBoard, gameStatus, startGame, startGameButton, resetGame, restartButton }
 })();
